12365121567dbd536e844f84ce486942dc6dd68f
Arpitagarwala <arpitagarwalms@gmail.com>
2026-04-08 11:33:51 +0530

    fix: Correct postinstall script path so it effectively patches node-pandas on Vercel

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,8 +4,8 @@ const path = require('path');
 console.log('Running postinstall script to patch node-pandas case-sensitivity bug...');
 
 const targetFiles = [
-  path.join(__dirname, 'node_modules', 'node-pandas', 'src', 'utils', 'getTransformedDataList.js'),
-  path.join(__dirname, 'node_modules', 'node-pandas', 'src', 'utils', 'utils.js')
+  path.join(__dirname, '..', 'node_modules', 'node-pandas', 'src', 'utils', 'getTransformedDataList.js'),
+  path.join(__dirname, '..', 'node_modules', 'node-pandas', 'src', 'utils', 'utils.js')
 ];
 
 targetFiles.forEach(file => {
