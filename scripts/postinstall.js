ca9c34a5fc3580e8f31a2da7c46edb99a5093818
Arpitagarwala <arpitagarwalms@gmail.com>
2026-04-08 11:30:49 +0530

    fix: Patch node-pandas case-sensitivity bug on Linux for Vercel deployment

---
+++ scripts/postinstall.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+console.log('Running postinstall script to patch node-pandas case-sensitivity bug...');
+
+const targetFiles = [
+  path.join(__dirname, 'node_modules', 'node-pandas', 'src', 'utils', 'getTransformedDataList.js'),
+  path.join(__dirname, 'node_modules', 'node-pandas', 'src', 'utils', 'utils.js')
+];
+
+targetFiles.forEach(file => {
+  if (fs.existsSync(file)) {
+    let content = fs.readFileSync(file, 'utf8');
+    // Replace case-sensitive requires that fail on Linux
+    content = content.replace(/require\(['"]\.\.\/utils\/dataType['"]\)/g, "require('../utils/datatype')");
+    content = content.replace(/require\(['"]\.\/dataType['"]\)/g, "require('./datatype')");
+    fs.writeFileSync(file, content, 'utf8');
+    console.log(`Patched: ${file}`);
+  } else {
+    console.warn(`File not found, skipping patch: ${file}`);
+  }
+});
+
+console.log('Postinstall patch complete.');
